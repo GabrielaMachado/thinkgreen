4fd639546de88f5e37b2b8dabdd95ded6418baf5
GabrielaMachado <gabimachadop@hotmail.com>
2019-05-15 16:21:04 -0500

    te amo bebecito

---
+++ src/app/models/Cliente.ts
@@ -0,0 +1,10 @@
+import { Usuario } from './Usuario';
+
+export class Cliente extends Usuario{
+    telefono: string;
+    edad: number;
+    direccion: string;
+    pais: string;
+    ciudad: string;
+    tipo: boolean;
+}
