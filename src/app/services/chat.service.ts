9a386a673624dc6f9666076dda1dc186b5b7a7be
DanielaDiazP <dani092012@hotmail.com>
2019-05-27 16:05:37 -0500

    notificacion x4

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -5,8 +5,10 @@ import { AuthService } from './auth.service';
 import { Mensaje } from "../interface/mensaje.interface";
 import { AngularFireAuth } from '../../../node_modules/@angular/fire/auth';
 import * as firebase from 'firebase/app';
+//import { Observable } from '../../../node_modules/rxjs';
 
-
+//export interface Cliente {nombre:string}
+//export interface Cliente {cedula:string}
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +16,25 @@ import * as firebase from 'firebase/app';
 export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  //private clientesCollection: AngularFirestoreCollection<Cliente>;
+  //cliente: Observable<Cliente[]>
+  //clientes:any;
   public chats: Mensaje[] = [];
   public usuario: any = {};
 
   constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) { 
+    //this.clientesCollection = afs.collection<Cliente>('clientes');
+    //this.cliente= this.clientesCollection.valueChanges();
+    //this.clientes.subscribe(cliente=>{
+      //this.clientes = cliente;
+    //})
     this.afAuth.authState.subscribe(user =>{
       console.log('Estado del usuario ', user);
       if(!user){
+        //console.log('este es el nombreee ',this.clientes.nombre);
         return;
+        //this.usuario.nombre = this.clientes.nombre;
+        //this.usuario.uid = this.clientes.cedula;
       }
       this.usuario.nombre = user.displayName;
       this.usuario.uid = user.uid;
